fc063dda8739f63207f4171e4b9ab437cc2a2cdd
chfw <wangc_2011@hotmail.com>
2019-05-27 23:11:05 +0100

    :fire: remove useless task

--- build/tasks/geojson.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const gulp = require('gulp');
-const maker = require("echarts-mapmaker/src/maker");
-const parser = require("echarts-mapmaker/src/parseGeoJson");
-const mapshaper = require('mapshaper');
